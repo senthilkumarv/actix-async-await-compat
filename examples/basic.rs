082f3cfa34d04ed6e50f060e9af10c1c589ee94f
Senthil Kumar Vijaya Kumar <senthil.vijay.kumar@gmail.com>
2019-11-09 11:36:53 +0530

    Some renames and fixes

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,21 +1,23 @@
-use actix_web::{App, Responder, HttpServer, web, web::Path};
+use std::time::{Duration, Instant};
+
+use actix_web::{App, HttpServer, Responder, web, web::Path};
 use failure::Error;
-use actix_web_async_await::{compat};
-use std::time::{Instant, Duration};
 use tokio_timer::delay;
 
+use actix_async_await_compat::wrap_async;
+
 async fn index(seconds: Path<u64>) -> Result<impl Responder, Error> {
-    let delay_seconds = seconds.into_inner();
-    delay(Instant::now() + Duration::from_secs(delay_seconds)).await;
-    Ok(format!("Response after {}", delay_seconds))
+  let delay_seconds = seconds.into_inner();
+  delay(Instant::now() + Duration::from_secs(delay_seconds)).await;
+  Ok(format!("Response after {}", delay_seconds))
 }
 
 fn main() {
-    let _ = HttpServer::new(|| {
-        App::new()
-            .service(web::resource("/index/{delay}").route(web::get().to(compat(index))))
-    })
-        .bind("0.0.0.0:8000")
-        .unwrap()
-        .run();
+  let _ = HttpServer::new(|| {
+    App::new()
+        .service(web::resource("/index/{delay}").route(web::get().to(wrap_async(index))))
+  })
+      .bind("0.0.0.0:8000")
+      .unwrap()
+      .run();
 }
